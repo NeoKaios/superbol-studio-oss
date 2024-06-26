39cc91c9a0398d9a7f803b4d217f5db723c0d4b3
NeoKaios <74507788+NeoKaios@users.noreply.github.com>
2024-06-26 10:42:04 +0000

    Deploying to gh-pages from  @ 60fa89a0813069f52fb5ef6da0e5b94543ab696f 🚀

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (60fa89a) (2024/06/26 10:42)',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
